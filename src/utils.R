474390ca03ae78e34d4aa0caee19f3afe423d4eb
Vik <viknesh.gopal@gmail.com>
2024-11-03 15:28:15 +0800

    topic 4 slides (cda) done

--- src/utils.R
+++ src/utils.R
@@ -6,5 +6,23 @@ qmd_nb <- function(infile, outfile) {
 }
 
 
+## Extract R codes
+r_codes <- c("01-intro_to_R.qmd",
+             "03-summarising_data.qmd", "04-categorical_data_analysis.qmd",
+             "05-robust_statistics.qmd", "07-2_sample_tests.qmd",
+             "08-anova.qmd", "09-regression.qmd", "10-simulation.qmd")
+infiles <- file.path("..", r_codes)
+sapply(infiles, knitr::purl)
+
+## Extract python codes
+py_codes <- c("02-intro_to_python.qmd",
+             "03-summarising_data.qmd", "04-categorical_data_analysis.qmd",
+             "05-robust_statistics.qmd", "07-2_sample_tests.qmd",
+             "08-anova.qmd", "09-regression.qmd", "10-simulation.qmd")
+infiles <- file.path("..", py_codes)
+root_names <- sapply(py_codes, remove_extension)
+outfiles <- paste0(unname(root_names), ".py")
+out <- mapply(qmd_nb, infiles, outfiles)
+
 # qmd_nb("intro_to_python_slides2.qmd", "test.qmd")
 # quarto convert test.qmd
