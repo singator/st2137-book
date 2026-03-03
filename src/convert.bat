ddba521cfe5316c3c1acb696d6885e3612c4c0c5
Vik <viknesh.gopal@gmail.com>
2026-03-04 04:52:48 +0800

    corrected formula for winsorised mean

---
+++ src/convert.bat
@@ -0,0 +1,8 @@
+quarto convert 02-intro_to_python.py
+quarto convert 03-summarising_data.py
+quarto convert 04-categorical_data_analysis.py
+quarto convert 05-robust_statistics.py
+quarto convert 07-2_sample_tests.py
+quarto convert 08-anova.py
+quarto convert 09-regression.py
+quarto convert 10-simulation.py
